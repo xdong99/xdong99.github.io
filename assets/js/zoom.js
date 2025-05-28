dcd7e70f9ee083fa205647852367d9a9f9468eab
xdong94 <xdong94@wisc.edu>
2025-05-28 17:40:53 -0500

    1

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
   medium_zoom = mediumZoom("[data-zoomable]", {
     background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
   });
+  medium_zoom.on('opened', (zoomedImage) => {
+  zoomedImage.classList.remove("rounded");
+  zoomedImage.style.borderRadius = "0";
+  });
 });
